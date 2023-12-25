c976fa5e2761e05635a9b2732093ce585ebf6d1b
jixcan <iamjixcan@gmail.com>
2023-12-25 15:08:32 +0300

    Added moves input

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
-let piece = ''; // переменная для фигуры
-let vertical = ''; // переменная для вертикали
-let horizontal = ''; // переменная для горизонтали
+const { Chess } = require('chess.js')
 
-const buttons = document.querySelectorAll('.buttons button');
+let figure = '';
+let vertical = '';
+let horizontal = '';
+
+const buttons = document.querySelectorAll('button');
+const textarea = document.querySelector('textarea');
+const messagesArray = [];
+
+let currentMove = ''; // Хранилище текущего хода
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const buttonType = button.parentNode.className;
+    const value = button.textContent;
 
-    // Обработка фигур
-    if (buttonType === 'pieces') {
-      // Проверяем, если фигура уже выбрана, сбрасываем
-      if (piece === button.textContent) {
-        piece = '';
-      } else {
-        piece = button.textContent;
-      }
+    if (['N', 'B', 'R', 'Q', 'K'].includes(value)) {
+      figure = value;
+    }
+    else if (['P'].includes(value)) {
+      figure = '';
+    } else if (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].includes(value)) {
+      vertical = value;
+    } else if (['1', '2', '3', '4', '5', '6', '7', '8'].includes(value)) {
+      horizontal = value;
     }
 
-    // Обработка вертикалей
-    if (buttonType === 'verticals') {
-      // Проверяем, если вертикаль уже выбрана, сбрасываем
-      if (vertical === button.textContent) {
-        vertical = '';
-      } else {
-        vertical = button.textContent;
-      }
-    }
-
-    // Обработка горизонталей
-    if (buttonType === 'horizontals') {
-      // Проверяем, если горизонталь уже выбрана, сбрасываем
-      if (horizontal === button.textContent) {
-        horizontal = '';
-      } else {
-        horizontal = button.textContent;
-      }
-    }
-
-    // Формирование записи и вывод на экран
-    const move = piece + (piece !== '' && vertical !== '' ? vertical : '') + (horizontal !== '' ? horizontal : '');
-    
-    // Вывод на экран текстового окна
-    const output = document.querySelector('.chat textarea');
-    output.value = move;
+    currentMove = `${figure}${vertical}${horizontal}`; // Обновление текущего хода
+    updateTextarea();
   });
-
-  
 });
 
+const submitButton = document.getElementById('submitMove');
+
+submitButton.addEventListener('click', () => {
+  if (currentMove.trim() !== '') {
+    addMessage(currentMove);
+    currentMove = '';
+    updateTextarea();
+    figure = vertical = horizontal = '';
+  } else {
+    textarea.value = 'Вы не ввели ход!';
+  }
+});
+
+function addMessage(message) {
+  messagesArray.push(message);
+}
+
+// После обновления textarea добавьте/удалите класс для анимации мигающего курсора
+function updateTextarea() {
+  const allMoves = messagesArray.join('\n');
+  if (allMoves.length !== 0) {
+    textarea.value = allMoves + (currentMove ? '\n' + currentMove : ''); // Отображение текущего хода
+  } else {
+    textarea.value = currentMove ? '\n' + currentMove : '';
+  }
+}
 
